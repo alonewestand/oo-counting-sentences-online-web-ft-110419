1b7d833bb88c8bfa35e7586094954149b367df4c
Ken Goad <goadke@g.cofc.edu>
2019-11-14 21:39:35 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -27,9 +27,9 @@ if string_self.end_with?("!")
   end
 
   def count_sentences
-     string_self.squeeze('.!?').count('.!?')
+     string_self.squeeze('.!?').count('.')
      
      #string_self.squeeze('.!?') searches the "string" of .count_sentences for duplicate #characters outlined in the argument and replaces all instances of duplicates with just one #of that character.
-     #.count then counts the punctuation remaining in string. Since 
+     #.count then counts the punctuation remaining in string. Since all concurrent periods have been removed, it accounts for sentences ending with ellipses. 
   end
 end
